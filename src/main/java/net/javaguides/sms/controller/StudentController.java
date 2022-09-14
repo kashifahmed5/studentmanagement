0c3f62ae8b3b30374e753c8a711dc099fac1ac4b
kashifahmed5 <71342921+kashifahmed5@users.noreply.github.com>
2022-09-14 13:35:03 +0500

    update

--- src/main/java/net/javaguides/sms/controller/StudentController.java
+++ src/main/java/net/javaguides/sms/controller/StudentController.java
@@ -9,7 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
 import net.javaguides.sms.entity.Student;
 import net.javaguides.sms.service.StudentService;
 
@@ -76,21 +81,38 @@ public class StudentController {
 		return "redirect:/students";		
 	}
 	
-	@PutMapping("/students/{id}")
-	public long updateStudents(@PathVariable Long id,
+//	@PutMapping("/students/{id}")
+//	public long updateStudents(@PathVariable Long id,
+//			@RequestBody Student student,
+//			Model model) {
+//		
+//		// get student from database by id
+//		Student existingStudent = studentService.getStudentById(id);
+//		existingStudent.setId(id);
+//		existingStudent.setFirstName(student.getFirstName());
+//		existingStudent.setLastName(student.getLastName());
+//		existingStudent.setEmail(student.getEmail());
+//		
+//		// save updated student object
+//		studentService.updateStudent(existingStudent);
+//		return student.getId();		
+//	}
+	@PutMapping("/students")
+	@ResponseBody
+	public String updateStudentss(@RequestParam Long id,
 			@RequestBody Student student,
 			Model model) {
 		
 		// get student from database by id
 		Student existingStudent = studentService.getStudentById(id);
-		existingStudent.setId(id);
+//		existingStudent.setId(id);
 		existingStudent.setFirstName(student.getFirstName());
 		existingStudent.setLastName(student.getLastName());
 		existingStudent.setEmail(student.getEmail());
 		
 		// save updated student object
 		studentService.updateStudent(existingStudent);
-		return student.getId();		
+		return "redirect:/students";	
 	}
 	
 	// handler method to handle delete student request
@@ -100,12 +122,20 @@ public class StudentController {
 		return "redirect:/students";
 	}
 	
-	
-	// handling delete with delete method
-	@DeleteMapping("/students/{id}")
-	public String deleteStudents(@PathVariable Long id) {
+	@DeleteMapping("/students")
+	@ResponseBody
+	public String deleteStudents(@RequestParam(required = false) Long id) {
 		studentService.deleteStudentById(id);
 		return "redirect:/students";
-	}
+	} 
+	
+	
+	
+	// handling delete with delete method
+//	@DeleteMapping("/students/{id}")
+//	public String deleteStudents(@PathVariable Long id) {
+//		studentService.deleteStudentById(id);
+//		return "redirect:/students";
+//	}
 	
 }
